fe03dbf022922637b75b82a5a262c0882263394f
EcMangawang <emmari.mangawang@edu.sait.ca>
2022-01-16 15:59:29 -0700

    Default result message
    If no calculation is added,  result shown is "---"

--- src/main/java/ca/sait/calculator/servlets/ArithmeticCalculatorServlet.java
+++ src/main/java/ca/sait/calculator/servlets/ArithmeticCalculatorServlet.java
@@ -23,6 +23,7 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+            request.setAttribute("message", "---");
             getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
     }
 
